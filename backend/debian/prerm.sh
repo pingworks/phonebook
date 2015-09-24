708c33ada33c9ddc128894a7a9ac36a310bae274
Christoph Lukas <lukas@pingworks.de>
2015-09-24 11:39:27 +0200

    use initscript in prerm

--- backend/debian/prerm.sh
+++ backend/debian/prerm.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 # kill running ruby app
-pid=$(ps xa \
-	| grep 'ruby2.1 /opt/phonebook-backend/vendor/bundle/ruby/2.1.0/bin/rackup' \
-	| grep -v grep \
-	| awk '{print $1}')
-kill $pid
+/etc/init.d/phonebook-backend stop
 
--- frontend/debian/prerm.sh
+++ frontend/debian/prerm.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 # kill running ruby app
-pid=$(ps xa \
-	| grep 'ruby2.1 /opt/phonebook-frontend/vendor/bundle/ruby/2.1.0/bin/rackup' \
-	| grep -v grep \
-	| awk '{print $1}')
-kill $pid
+/etc/init.d/phonebook-frontend stop
 
