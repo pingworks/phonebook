6b83f1e2f134afedc3857e849055fdb52fcd5dea
Christoph Lukas <lukas@pingworks.de>
2016-10-04 18:23:54 +0200

    fixed path

--- kubernetes/build-pod-build-and-test.sh
+++ kubernetes/build-pod-build-and-test.sh
@@ -3,5 +3,5 @@
 ${WORKSPACE}/../kube/kubectl run phonebook-build \
   --image=kube-registry.kube-system.svc.cluster.local:5000/ruby-phonebook:a34121390db3 \
   --restart=Never \
-  --overrides="$(<build-pod-overrides.json)" 
+  --overrides="$(<kubernetes/build-pod-overrides.json)" 
 
