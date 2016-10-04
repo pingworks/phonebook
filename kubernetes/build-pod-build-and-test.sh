9243a2538635278c1470cf6221c3171d947b6b28
Christoph Lukas <lukas@pingworks.de>
2016-10-04 18:36:04 +0200

    added parameters for pkg and ver

--- kubernetes/build-pod-build-and-test.sh
+++ kubernetes/build-pod-build-and-test.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+pkg=$1
+ver=$2
+if [ -z "$pkg" -o -z "$ver" ]; then
+  echo "Usage $0 <frontend|backend> <version>"
+  exit 1
+fi
+
+
 ${WORKSPACE}/../kube/kubectl run phonebook-build \
   --image=kube-registry.kube-system.svc.cluster.local:5000/ruby-phonebook:a34121390db3 \
   --restart=Never \
-  --overrides="$(<kubernetes/build-pod-overrides.json)" 
+  --overrides="$(sed -e "s;__PKG__;$pkg;g" \
+	-e "s;__VERSION__;$ver;g' \
+	kubernetes/build-pod-overrides.json)
 
