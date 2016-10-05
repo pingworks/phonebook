279b56b07453b165d6a8a7f3e86f19cd9160dfa3
Christoph Lukas <lukas@pingworks.de>
2016-10-05 13:59:54 +0200

    added chdir before test

--- kubernetes/inttest.sh
+++ kubernetes/inttest.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+dir=$1
+if [ -z "$dir" -o ! -d "$dir" ]; then
+	echo "Usage: $0 <directory>"
+fi
+
 # Run integration tests
+cd $dir
 bundle exec rspec \
 	--format RspecJunitFormatter \
 	--out coverage/rspec.xml
